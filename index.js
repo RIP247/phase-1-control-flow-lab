ec3b2cabc1c3f450a20cd84fd2a6e68cf0baa4df
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-01-30 19:43:51 -0500

    commit tree 57fee7e499f8e41c6f8606bcb6683fb7b2d4ad27

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber <= 400){
+    result = 'This one is on me!';
+  }
+  else if (someNumber >= 2500) {
+    result = 'No can do.';
+   }
+  else if (someNumber > 2000){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  return result;
+  }
+ 
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : 'No go.'
 }
 
 function switchOnCharmFromTip(){
